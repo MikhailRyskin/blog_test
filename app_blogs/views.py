ff199e0eeade0f27b58e5b3e254c3a501e8cfabc
rymv <rymv@yandex.ru>
2021-12-02 19:44:46 +0300

    add sub, feed, login, logout

--- app_blogs/views.py
+++ app_blogs/views.py
@@ -1,5 +1,6 @@
+from django.shortcuts import redirect
 from django.views.generic import ListView, DetailView
-from app_blogs.models import Blog, Post
+from app_blogs.models import Blog, Post, Subscription, Read
 
 
 class BlogListView(ListView):
@@ -23,6 +24,38 @@ class BlogItemView(ListView):
         context["blog"] = Blog.objects.get(pk=self.kwargs["pk"])
         return context
 
+    def post(self, *args, **kwargs):
+        Subscription.objects.create(
+            user=self.request.user,
+            blog=Blog.objects.get(pk=self.kwargs["pk"])
+        )
+        print('записали подписку в модель')
+        return redirect('blog_list')
+
 
 class PostDetailView(DetailView):
     model = Post
+
+    def post(self, *args, **kwargs):
+        Read.objects.create(
+            user=self.request.user,
+            post=Post.objects.get(pk=self.kwargs["pk"])
+        )
+        print('пометили пост как прочитанный')
+        return redirect('blog_list')
+
+
+class FeedListView(ListView):
+    model = Post
+    context_object_name = "feed_posts"
+    template_name = "app_blogs/feed_list.html"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        in_subscriptions = Subscription.objects.filter(user=self.kwargs["pk"])
+        blogs_in_sub = []
+        for sub in in_subscriptions:
+            blogs_in_sub.append(sub.blog_id)
+        # print(blogs_in_sub)
+        queryset = queryset.filter(blog_id__in=blogs_in_sub)
+        return queryset
--- app_users/urls.py
+++ app_users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from app_users.views import AccountView, AnotherLoginView, AnotherLogoutView
+from app_blogs.views import FeedListView
 
 urlpatterns = [
     path('login', AnotherLoginView.as_view(), name='login'),
     path('logout', AnotherLogoutView.as_view(), name='logout'),
     path('<int:pk>', AccountView.as_view(), name='account'),
+    path('feed/<int:pk>', FeedListView.as_view(), name='feed_list'),
 ]
